90e70e465c6c976bd3f68f0bc710249bedac83fd
Joseph Nichols <joseph@skuid.com>
2018-10-17 11:28:55 -0400

    Almost finished

--- modal/js/default.js
+++ modal/js/default.js
@@ -1,19 +1,40 @@
 
+/* Button Row Template */
+var button_row = $(".row");
+
+
 $(function() {
-    //console.log( "ready!" );
+
+	
 
 	function openModal(){
 		$("#myModal").addClass("show");
 	}
 	function closeModal(){
 		$("#myModal").removeClass("show");
+		$(".modal-footer .button-group").empty();
+	}
+	function helloWorld() {
+		alert("Hello World!");
 	}
 
 
-	$("#myBtn").on("click", function(){
-		openModal();
-	});
 
+
+	function addRow() {
+		button_row.clone(true, true).appendTo("#buttonRows");
+	}
+	function removeRow(button) {
+		button.closest(".row").remove();
+	}
+
+	$(".add").on("click", function(){
+		addRow();
+		event.preventDefault();
+	});
+	$(".remove").on("click", function(e){
+		removeRow( e.target);
+	});
 
 	// Close when close button is clicked
 	$("#closeBtn").on("click", function(){
@@ -29,13 +50,84 @@ $(function() {
 	});
 
 
-	//Set to close when the document is clicked anywhere...
-	$(document).click(function() {
-	    closeModal();
+	//Set to close when the document is clicked anywhere...Except for when the modal itself is clicked
+	$(document).click(function() { closeModal(); });
+	$("#myModal").click(function(event) { event.stopPropagation(); });
+
+
+
+	$( "form" ).submit(function( event ) {
+
+
+		//Set the title
+		$( "#_title" ).text( $('input[name="title"]').val() || "No title provided" );
+
+
+		//Set the content
+		$( "#_content" ).html( $('input[name="content"]').val() || "No content provided" );
+		//TODO: Depending on the context of this form, we may need to sanitize the html input to safeguard against malicious script insertions!!!
+
+
+		//Add the buttons
+		var button_labels = $('input[name="button-label"]');
+		var button_styles = $('select[name="button-style"]');
+		var button_callbacks = $('select[name="button-callback"]');
+
+		//console.log(button_labels[0].value);
+
+		var arrayLength = button_labels.length;
+		for (var i = 0; i < arrayLength; i++) {
+			//console.log(button_labels[i].value + "|" + button_styles[i].value + "|" + button_callbacks[i].value);
+		    var btn = $('<button class="button">' + button_labels[i].value + '</button>');
+		    if (button_styles[i].value == "primary") btn.addClass("button--primary");
+		    btn.on("click", eval(button_callbacks[i].value) );
+		    $(".modal-footer .button-group").append(btn);
+		}
+
+
+
+
+
+
+	  var fields = $( this ).serializeArray();
+
+	  console.log(fields);
+
+	  $.each( fields, function( i, field ) {
+	    //console.log( field.name + " = " + field.value);
+	    if (field.name == "title") {
+		    $( "#_title" ).text( field.value || "Default " + field.name );
+		}
+		else if (field.name == "content") {
+		    $( "#_content" ).html( field.value || "Default " + field.name );
+		}
+		// else if (field.name == "button-label") {
+		//     //$( "#_" + field.name ).html( field.value || "Default " + field.name );
+		//     var btn = $('<button class="button">' + field.value + '</button>');
+		//     //btn.addClass("button--primary");
+		//     btn.on("click", closeModal);
+		//     $(".modal-footer .button-group").append(btn);
+		// }
+	    
+
+
+
+	 });
+
+
+
+
+
+
+
+
+
+	  
+	  openModal();
+	  
+	  event.preventDefault();
 	});
-	//...Except for when the modal itself is clicked
-	$("#myModal,#myBtn").click(function(event) {
-	    event.stopPropagation();
-	});
+
+
 
 });
